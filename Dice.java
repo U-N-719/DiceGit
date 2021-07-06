62b2fe7f5468735cb3113d15006f45d49647b82d
U-N-719 <president.fuller@gmail.com>
2021-07-06 18:31:36 +0900

    Won/lost message added

--- Dice.java
+++ Dice.java
@@ -9,6 +9,14 @@ public class Dice{
 	int val2 = (int)(Math.random() * 6 )+1;
 	System.out.println("Die 2: "+val2);
 	System.out.println("Total value: " + (val1+val2));
+	
+	int all = val1+val2;
+	if(all>7){
+	    System.out.println("You won.");
+	}else{
+	    System.out.println("You lost.");
+	}
 
+	
     }
 }
